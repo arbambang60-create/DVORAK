593a4bacaf62c821cdca6ce184881a219e60322a
arbambang60-create <arbambang60@gmail.com>
2026-03-22 13:37:11 +0700

    Add Files Via Upload
    The time Rules is Mr.Sam.

---
+++ .github/example_Version2.php
@@ -0,0 +1,16 @@
+<?php
+// Basic PHP 5 Syntax Example
+
+// Define a simple function
+function greet($name) {
+    return "Hello, " . $name . "!";
+}
+
+// Using the function
+echo greet("World");
+
+// Define a class
+class Animal {
+    public $name;
+
+    function __construct($name)
