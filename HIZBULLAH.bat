ff254359afaedf6ae09773b298922ffe0b98b005
arbambang60-create <arbambang60@gmail.com>
2026-04-20 08:29:59 +0700

    Add files via upload

---
+++ HIZBULLAH.bat
@@ -0,0 +1,27 @@
+@echo off
+mode 70,10
+title GARIS_BESAR
+
+
+
+@echo sampah Yang ada Dalam parlindungan semarakkan Hari ini!
+
+
+
+@echo FreePalestine
+timeout 7 >nul
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Text Document.bat
@@ -0,0 +1,23 @@
+@echo off
+
+
+title DASAWARSA
+mode 82,71
+
+@echo.
+@echo.
+@echo.
+color D
+@echo Aku Ingin pintar.
+@echo Selalu Ada....!
+@echo.
+
+
+
+
+
+
+
+
+
+pause
